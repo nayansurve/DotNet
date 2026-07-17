39cf20dd11a7fec17ff74fba1fdcf0377b802510
Nayana Surve <survenayan870@gmail.com>
2026-07-17 11:20:27 +0530

    Static KeyWord Understanding

---
+++ DotNetOOPs/BasicC#/StaticKeyWord Understanding/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel;
+
+public class StaticKeyword
+{
+    public static int Add(int a,int b)
+    {
+        return a+b;
+    }
+
+}
+
+public class Program
+{
+    public static void Main(string [] args )
+    {
+         int result=StaticKeyword.Add(10,20);
+         Console.WriteLine($"Addition = {result}");
+
+    }
+
+}
