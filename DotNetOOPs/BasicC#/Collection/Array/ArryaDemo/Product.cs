8ee21ad2ef900e2100df643fd236ec52380bbc4c
Nayana Surve <survenayan870@gmail.com>
2026-07-20 18:11:15 +0530

    Collection using simple single array

---
+++ DotNetOOPs/BasicC#/Collection/Array/ArryaDemo/Product.cs
@@ -0,0 +1,18 @@
+namespace CollectionDemo;
+
+public class Product
+{
+    public int Id;
+
+    public string Name;
+
+    public int Price;
+
+    public Product(int id,string name,int price)
+    {
+        this.Id=id;
+        this.Name=name;
+        this.Price=price;
+    }
+
+}
---
+++ DotNetOOPs/BasicC#/Collection/Array/ArryaDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using CollectionDemo;
+
+public class Program
+{
+    public static void Main(string [] args)
+    {
+        Product[] products=new Product[5]
+        {
+            
+              new Product(101, "Mouse", 500),
+              new Product(102, "Keyboard",700),
+              new Product(103,"LAN",1000),
+              new Product(104,"Laptop",2000),
+              new Product(105,"Charger",4000),
+        };
+    
+
+    
+          Console.WriteLine($"Product Name : {products[0].Id}");
+          Console.WriteLine($"Product name : {products[0].Name}");
+          Console.WriteLine($"Product price : {products[0].Price}");
+        
+
+
+        
+
+}
+}
