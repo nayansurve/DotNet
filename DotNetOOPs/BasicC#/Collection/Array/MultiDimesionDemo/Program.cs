6e99e53482dd6e5ff600a1a5475d0e0bd51515ad
Nayana Surve <survenayan870@gmail.com>
2026-07-21 17:02:22 +0530

    MultiDimensionalArray

---
+++ DotNetOOPs/BasicC#/Collection/Array/MultiDimesionDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main(string [] args)
+    
+    {
+        int[,] matrix=new int[2,3]
+        {
+            {10,20,30},
+            {40,50,60}
+        };
+
+    //   Console.WriteLine("Value= "+ matrix[0,0]);
+    //   Console.WriteLine("Value = "+ matrix[0,1]);
+    //   Console.WriteLine("Value = "+ matrix[0,2]);
+    //   Console.WriteLine("Value = "+ matrix[1,0]);
+    //   Console.WriteLine("Value = "+ matrix[1,1]);
+    //   Console.WriteLine("Value = "+ matrix[1,2]);
+
+    for(int i=0;i<2;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                Console.Write(matrix[i,j]+"\t");
+            }
+            Console.WriteLine();
+        }
+      
+    }
+}
