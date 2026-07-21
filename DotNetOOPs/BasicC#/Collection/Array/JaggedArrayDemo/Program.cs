6603da7b9a8850bdce1a439d1431ef33893c2d59
Nayana Surve <survenayan870@gmail.com>
2026-07-21 17:24:11 +0530

    JaggedArrayDemo

---
+++ DotNetOOPs/BasicC#/Collection/Array/JaggedArrayDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace JaggedArrayDemo;
+
+public class Program
+{
+    public static void Main(string [] args)
+    {
+        int[][] numbers=new int[3][];
+
+        numbers[0]=new int[]{10,20,30};
+        numbers[1]=new int[]{40,50,60,70};
+        numbers[2]=new int[]{80,90};
+
+        for(int i=0;i<numbers.Length;i++)
+        {
+            for(int j=0;j<numbers[i].Length;j++)
+            {
+                Console.Write(numbers[i][j]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
