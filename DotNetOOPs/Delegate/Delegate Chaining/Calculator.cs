c1fb09325acd98427b252788c0005c62bd2bb06f
Nayana Surve <survenayan870@gmail.com>
2026-07-22 15:28:27 +0530

    Combining Delegate Example

---
+++ DotNetOOPs/Delegate/Delegate Chaining/Calculator.cs
@@ -0,0 +1,16 @@
+namespace ChainingDemo;
+delegate void CalDelegate();
+public class Calculator
+{
+
+    public void Add()
+    {
+        Console.WriteLine("Add Method Called Here !!!");
+    }
+
+    public void Sub()
+    {
+        Console.WriteLine("Sub Method Called Here !!!");
+    }
+    
+}
---
+++ DotNetOOPs/Delegate/Delegate Chaining/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Buffers;
+using ChainingDemo;
+
+public class Program
+{
+    public static void Main(string [] args)
+    {
+        Calculator cal=new Calculator();
+
+        CalDelegate add=cal.Add;
+
+        CalDelegate sub=cal.Sub;
+
+        CalDelegate combine;
+
+        combine=(CalDelegate)Delegate.Combine(add,sub);
+        
+        Console.WriteLine("Combining two methods");   
+
+        combine();
+
+        combine=(CalDelegate)Delegate.Remove(combine,sub);
+       // combine=(CalDelegate)Delegate.Remove(combine,add);
+
+        Console.WriteLine("After Removing one method");
+
+        combine();
+    }
+}
