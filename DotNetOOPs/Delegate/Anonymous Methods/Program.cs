642946be02e6a2ac8895ea2cbd6fd42f429cbb81
Nayana Surve <survenayan870@gmail.com>
2026-07-22 16:17:57 +0530

    Anonymous Method

---
+++ DotNetOOPs/Delegate/Anonymous Methods/Program.cs
@@ -0,0 +1,15 @@
+﻿//An Anonymous Method is an unnamed method used when we don't want to create a separate method
+delegate string MyDelegate(string str);
+public class Program
+{
+    public static void Main(string [] args)
+    {
+        MyDelegate del=delegate(string str)
+        {
+           return str.ToUpper();
+        };
+
+        Console.WriteLine(del("TransFlower"));
+        
+    }
+}
