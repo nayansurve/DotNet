d201036d54bcf21c079cac5b1b007f0e44edbb78
Nayana Surve <survenayan870@gmail.com>
2026-07-22 16:30:47 +0530

    Built In Delegate Understanding

---
+++ DotNetOOPs/Delegate/BuiltInDelegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Action<string> greet =
+        delegate(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        };
+
+        Func<int,int,int> add =
+        delegate(int a,int b)
+        {
+            return a+b;
+        };
+
+        Predicate<int> even =
+        delegate(int n)
+        {
+            return n%2==0;
+        };
+
+        greet("Nayan");
+
+        Console.WriteLine(add(10,20));
+
+        Console.WriteLine(even(10));
+    }
+}
