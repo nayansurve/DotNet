a1d64b74daef9d93b3f84f185e8fd93cc1625350
Nayana Surve <survenayan870@gmail.com>
2026-07-23 15:48:05 +0530

    StudentApp

---
+++ DotNetOOPs/Reflection In C#/StudentApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+
+class Student
+{
+    public string Name { get; set; } = "Nayan";
+
+    public int Age { get; set; } = 22;
+
+    public Student()
+    {
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Welcome " + Name);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Step 1: Create object
+        Student student = new Student();
+
+        // Step 2: Get type information
+        Type type = student.GetType();
+
+        Console.WriteLine("Class Name : " + type.Name);
+
+        Console.WriteLine("\nProperties");
+
+        foreach (PropertyInfo property in type.GetProperties())
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        Console.WriteLine("\nMethods");
+
+        foreach (MethodInfo method in type.GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
+
+        Console.WriteLine("\nConstructors");
+
+        foreach (ConstructorInfo constructor in type.GetConstructors())
+        {
+            Console.WriteLine(constructor.Name);
+            Console.WriteLine(type.Name);
+        }
+    }
+}
