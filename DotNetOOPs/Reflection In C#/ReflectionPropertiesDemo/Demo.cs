400b5fb368b476156b4f1985eafca3bbf3203d44
Nayana Surve <survenayan870@gmail.com>
2026-07-23 15:39:59 +0530

    RefelctionPropertiesDemo

---
+++ DotNetOOPs/Reflection In C#/ReflectionPropertiesDemo/Demo.cs
@@ -0,0 +1,24 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace Demoapp;
+
+public class Demo
+{
+    public int id{get;set;}
+
+ public string? Name {get;set;}
+
+    
+   
+
+    // public Demo(int Id,string Name)
+    // {
+    //     id=Id;
+    //     name=Name;
+    // }
+
+    // public void Display()
+    // {
+    //     Console.WriteLine("Displaying the properties using the Reflection");
+    // }
+}
---
+++ DotNetOOPs/Reflection In C#/ReflectionPropertiesDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Demoapp;
+using System;
+
+public  class Program
+{
+    public static void Main(string [] args)
+    {
+        Demo d=new Demo();
+
+        Type type=d.GetType();
+
+        var properties=type.GetProperties();
+        Console.WriteLine("Properties Displayed By the Reflction In C#");
+
+        foreach(var p in properties)
+        {
+        Console.WriteLine(p.Name);
+        }
+    }
+}
