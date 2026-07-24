d390165952a758e2dd7779b889389eef9268d1fd
Nayana Surve <survenayan870@gmail.com>
2026-07-24 11:10:00 +0530

    VowelsAndConsonentCount

---
+++ String/VowelsAndConsonent.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class VowelsAndConsonent
+{
+    public static void Main(string [] args)
+    {
+        Console.WriteLine("Enter A String = ");
+        string str=Console.ReadLine().ToLower();
+
+        int Vowels=0;
+        int consonent=0;
+
+        foreach(char ch in str)
+        {
+            if(ch >='a' && ch<='z')
+            {
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                {
+                    Vowels++;
+                }
+                else
+                {
+                    consonent++;
+                }
+            }
+        }
+        Console.WriteLine("Vovels Count ="+Vowels);
+         Console.WriteLine("Consonent Count ="+consonent);
+    }
+}
